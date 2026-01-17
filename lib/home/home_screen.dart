9dbdc6ad947509aac5fd9324ca0442c55b4fbfcd
elt <info@eltapp.com>
2026-01-17 16:02:43 +0530

    home

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -190,6 +190,27 @@ class HomeScreen extends StatelessWidget {
                         yearsDurationController.text,
                       ),
                     );
+                isDetailsButtonVisible = true;
+                if (isDetailsButtonVisible == true) {
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (context) => DetailedCalculation(
+                            tab: index,
+                          ),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'Detailed EMI',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                      ),
+                    ),
+                  );
+                }
               },
               child: const Text(
                 'Calculate',
@@ -333,6 +354,27 @@ class HomeScreen extends StatelessWidget {
                         yearsDurationController.text,
                       ),
                     );
+                isDetailsButtonVisible = true;
+                if (isDetailsButtonVisible == true) {
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (context) => DetailedCalculation(
+                            tab: index,
+                          ),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'Detailed Loan Amount',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                      ),
+                    ),
+                  );
+                }
               },
               child: const Text(
                 'Calculate',
@@ -476,6 +518,27 @@ class HomeScreen extends StatelessWidget {
                         emiController.text,
                       ),
                     );
+                isDetailsButtonVisible = true;
+                if (isDetailsButtonVisible == true) {
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (context) => DetailedCalculation(
+                            tab: index,
+                          ),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'Detailed Interest',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                      ),
+                    ),
+                  );
+                }
               },
               child: const Text(
                 'Calculate',
@@ -602,6 +665,27 @@ class HomeScreen extends StatelessWidget {
                         emiController.text,
                       ),
                     );
+                isDetailsButtonVisible = true;
+                if (isDetailsButtonVisible == true) {
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (context) => DetailedCalculation(
+                            tab: index,
+                          ),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'Detailed Period',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                      ),
+                    ),
+                  );
+                }
               },
               child: const Text(
                 'Calculate',
